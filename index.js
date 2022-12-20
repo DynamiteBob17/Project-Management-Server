e437350576fb43a1c8afad73657f3f0ec7af33e9
PickleEaterJim33 <jan.mlinaric@gmail.com>
2022-12-20 02:48:34 +0100

    revert: to 'Initial commit'

--- index.js
+++ index.js
@@ -7,18 +7,11 @@ const helmet = require('helmet');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 app.use(helmet());
 
 app.get('/', (req, res) => {
     res.status(200).send({ message: 'health check: api is up and running' });
-});
-
-const corsOptions = {
-    origin: 'https://pm-pej33.firebaseapp.com',
-    optionsSuccessStatus: 200
-}
-app.get('/secure', cors(corsOptions), (req, res) => {
-    res.status(200).send({ message: 'secure route' });
 });
 
 const routes = require('./api_routes/routes');
