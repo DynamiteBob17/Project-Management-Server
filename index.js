9ae6c5804a463503ec1ff42e058859eb2efebe56
Jan Mlinarić <jan.mlinaric@gmail.com>
2023-04-20 18:16:39 +0200

    chore: add morgan to index.js

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./api_routes/routes');
 
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
